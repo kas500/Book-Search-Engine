aa802c814b5dc1364fe64e60ea65ff85faa7e579
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-05 15:44:20 -0800

    Define user , auth , savedbooks querry

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,26 @@
+const typeDefs = `#graphql
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    savedBooks: [Book]!
+    book(bookId: ID!): Book
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
 
 module.exports = typeDefs;
