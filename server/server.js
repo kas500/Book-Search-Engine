2e7720d08007fdba0d54e9d30516c710ab17fb1a
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-05 14:00:13 -0800

    Adds func startApolloServer

--- server/server.js
+++ server/server.js
@@ -33,6 +33,21 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  db.once("open", () => {
+    httpServer.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
