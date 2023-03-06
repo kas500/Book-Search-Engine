5a310fe162a4798d401da8dcba88018907de956a
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-05 17:34:30 -0800

    Delete Rest API routes middle

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
@@ -31,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
