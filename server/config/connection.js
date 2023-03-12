577e1ea1223a827049aaed13aae50831efd530e4
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-12 01:16:07 -0800

    useCreateIndex: false

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
-  useCreateIndex: false,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
